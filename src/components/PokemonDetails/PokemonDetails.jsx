73b9c7b6a016491df6d9690150e095cd088c72c6
Landon-Spencer <landon.r.spencer@gmail.com>
2025-03-04 13:42:15 -0700

    removed duplicate types

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -20,30 +20,57 @@ export default function PokemonDetails() {
     }
   }, []);
 
+  // useEffect(() => {
+  //   if (pokemonInfo.types && pokemonInfo.types.length > 0) {
+  //     const typeUrls = pokemonInfo.types.map((t) => t.type.url);
+  //     Promise.all(
+  //       typeUrls.map((url) =>
+  //         fetch(url).then((res) => res.json())
+  //       )
+  //     )
+  //       .then((results) => {
+  //         const drArray = results.map((result) => result.damage_relations);
+  //         setDamageRelations(drArray);
+  //       })
+  //       .then(() => {
+
+  //       })
+  //       .catch((error) =>
+  //         console.error("Error fetching damage relations:", error)
+  //       );
+  //   }
+  // }, [pokemonInfo]);
+
   useEffect(() => {
-    if (pokemonInfo.types && pokemonInfo.types.length > 0) {
+    if (pokemonInfo?.types?.length > 0) {
       const typeUrls = pokemonInfo.types.map((t) => t.type.url);
-      Promise.all(
-        typeUrls.map((url) =>
-          fetch(url).then((res) => res.json())
-        )
-      )
+
+      Promise.all(typeUrls.map((url) => fetch(url).then((res) => res.json())))
         .then((results) => {
-          const drArray = results.map((result) => result.damage_relations);
-          setDamageRelations(drArray);
-        })
-        .then(() => {
+          const damageRelationsArray = results.map((result) => result.damage_relations);
+          setDamageRelations(damageRelationsArray);
+
+          setDoubleDamageFrom(removeDuplicates(damageRelationsArray.flatMap((dr) => dr.double_damage_from.map((d) => d.name))));
+
+          setDoubleDamageTo(removeDuplicates(damageRelationsArray.flatMap((dr) => dr.double_damage_to.map((d) => d.name))));
+
+          setHalfDamageFrom(removeDuplicates(damageRelationsArray.flatMap((dr) => dr.half_damage_from.map((d) => d.name))));
+
+          setHalfDamageTo(removeDuplicates(damageRelationsArray.flatMap((dr) => dr.half_damage_to.map((d) => d.name))));
 
         })
-        .catch((error) =>
-          console.error("Error fetching damage relations:", error)
-        );
+        .catch((error) => console.error("Error fetching damage relations:", error));
     }
   }, [pokemonInfo]);
 
+  const removeDuplicates = (inputArray) => {
+    let removedDuplicates = [...new Set(inputArray)];
+    return removedDuplicates;
+  }
+
   return (
     <>
-      <h1>PokemonDetails</h1>
+      <h1>{pokemonInfo.name}</h1>
       <div className='details-container'>
 
         <div className='pokemon-information'>
@@ -58,9 +85,9 @@ export default function PokemonDetails() {
         <div className='damage-relations'>
           <div className='strong-against'>
             <h3>Double Damage To</h3>
-            <p>something something</p>
+            <p>{doubleDamageTo.length ? doubleDamageTo.join(", ") : "None"}</p>
             <h3>Half Damage From</h3>
-            <p>something something</p>
+            <p>{halfDamageFrom.length ? halfDamageFrom.join(", ") : "None"}</p>
           </div>
           <div className='neutral-against'>
             <h3>Neutral</h3>
@@ -68,13 +95,13 @@ export default function PokemonDetails() {
           </div>
           <div className='weak-against'>
               <h3>Half Damage To</h3>
-              <p>something something</p>
+              <p>{halfDamageTo.length ? halfDamageTo.join(", ") : "None"}</p>
               <h3>Double Damage From</h3>
-              <p>something something</p>
+              <p>{doubleDamageFrom.length ? doubleDamageFrom.join(", ") : "None"}</p>
           </div>
         </div>
 
-        <div className="damage-relations">
+        {/* <div className="damage-relations">
         {damageRelations.length > 0 ? (
           damageRelations.map((dr, index) => (
             <div key={index} className="damage-relations-type">
@@ -100,7 +127,7 @@ export default function PokemonDetails() {
         ) : (
           <p>Loading damage relations...</p>
         )}
-      </div>
+      </div> */}
 
       </div>
 
